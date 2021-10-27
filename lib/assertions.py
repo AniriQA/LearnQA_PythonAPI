de52069eca51ab129d759e9f3d3b0af0ae820725
AniriQA <92028965+AniriQA@users.noreply.github.com>
2021-10-27 23:22:48 +0300

    репозиторий новый

--- lib/assertions.py
+++ lib/assertions.py
@@ -11,7 +11,38 @@ class Assertions:
         except json.JSONDecoderError:
             assert False, f"Ответ не в json формате. Текст ответа '{response.text}' "
 
-        assert  name in response_as_dict, f"Response JSON не иеет ключ '{name}'"
+        assert  name in response_as_dict, f"Response JSON не имеет ключ '{name}'"
 
         assert response_as_dict[name] == expected_value, error_message
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecoderError:
+            assert False, f"Ответ не в json формате. Текст ответа '{response.text}' "
+
+            assert  name in response_as_dict, f"Response JSON не имеет ключ '{name}'"
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names:list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecoderError:
+            assert False, f"Ответ не в json формате. Текст ответа '{response.text}' "
+        for name in names:
+            assert name in response_as_dict, f"Response JSON не имеет ключ '{name}'"
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecoderError:
+            assert False, f"Ответ не в json формате. Текст ответа '{response.text}' "
+
+        assert name not in response_as_dict, f"!!!Response JSON не имеет ключ '{name}'"
+
+    @staticmethod
+    def assert_code_status(response: Response, expected_status_code):
+        assert response.status_code == expected_status_code, f'Неожидаемый статус код {expected_status_code}, нужен {response.status_code}'
+
 
