2fac6700bd5af9c6518ceeed784892396f9375b7
AniriQA <92028965+AniriQA@users.noreply.github.com>
2021-11-04 19:54:41 +0300

    логгер с ошибкой

--- logger.py
+++ logger.py
@@ -1,10 +1,10 @@
 
 import datetime
 import os
+from requests import Response
 
-
-class logger:
-    file_name = f"logs/log_" + str(datetime.datetime.now()) + ".log"
+class Logger:
+    file_name = f"logs/log_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + ".log"
     @classmethod
     def _write_log_to_file(cls, data: str):
         with open(cls.file_name, 'a', encoding='utf-8') as logger_file:
@@ -14,7 +14,7 @@ class logger:
         testname = os.environ.get('PYTEST_CURRENT_TEST')
 
         data_to_add = f"\n-----\n"
-        data_to_add += f'Test:{testname}\n'
+        data_to_add += f"Test:{testname}\n"
         data_to_add += f"Time: {str(datetime.datetime.now())}\n"
         data_to_add += f"Request method: {method}\n"
         data_to_add += f"Request URL: {url}\n"
@@ -26,16 +26,16 @@ class logger:
         cls._write_log_to_file(data_to_add)
 
 
-        @classmethod
-        def add_response(cls, response: Response):
-            cookies_as_dict = dict(response.cookies)
-            headers_as_dict=dict(response.headers)
+    @classmethod
+    def add_response(cls, response: Response):
+        cookies_as_dict = dict(response.cookies)
+        headers_as_dict = dict(response.headers)
 
-            data_to_add = f"Response code: {response.status_code}\n"
-            data_to_add += f"Response text: {response.text}\n"
-            data_to_add += f"Response header: {headers_as_dict}\n"
-            data_to_add += f"Response cookies: {cookies_as_dict}\n"
-            data_to_add += "\n-----\n"
+        data_to_add = f"Response code: {response.status_code}\n"
+        data_to_add += f"Response text: {response.text}\n"
+        data_to_add += f"Response header: {headers_as_dict}\n"
+        data_to_add += f"Response cookies: {cookies_as_dict}\n"
+        data_to_add += "\n-----\n"
 
 
-            cls._write_log_to_file(data_to_add)
+        cls._write_log_to_file(data_to_add)
--- my_requests.py
+++ my_requests.py
@@ -1,22 +1,28 @@
 
 import requests
+from lib.logger import logger
+import allure
 
 class MyRequests():
     @staticmethod
     def post(url: str, data: dict=None, headers: dict = None, cookies: dict = None):
-        return MyRequests._send(url, data,headers,cookies, 'POST')
+        with allure.step(f"POST request to URL '{url}'"):
+            return MyRequests._send(url, data,headers,cookies, 'POST')
 
     @staticmethod
     def get(url:str, data: dict  =None, headers: dict = None, cookies: dict = None):
-        return MyRequests._send(url, data,headers,cookies, 'GET')
+        with allure.step(f"GET request to URL '{url}'"):
+            return MyRequests._send(url, data,headers,cookies, 'GET')
 
     @staticmethod
     def put(url:str, data:dict=None, headers: dict=None, cookies: dict=None):
-        return MyRequests._send(url, data,headers,cookies, 'PUT')
+        with allure.step(f"PUT request to URL '{url}'"):
+            return MyRequests._send(url, data,headers,cookies, 'PUT')
 
     @staticmethod
     def delete(url:str, data:dict=None, headers: dict=None, cookies: dict=None):
-        return MyRequests._send(url, data,headers,cookies, 'DELETE')
+        with allure.step(f"DELETE request to URL '{url}'"):
+            return MyRequests._send(url, data,headers,cookies, 'DELETE')
 
 
     @staticmethod
@@ -27,6 +33,8 @@ class MyRequests():
             headers = {}
         if cookies is None:
             cookies = {}
+
+        Logger.add_request(url, data, headers, cookies, method)
 
         if method =="GET":
             response=requests.get(url,params=data, headers=headers, cookies=cookies)
@@ -39,4 +47,5 @@ class MyRequests():
         else:
             raise Exception(f"плохой метод {method} был использован")
 
+        Logger.add_response(response)
         return response
